c1a0abb5fba019361692ec00df6cb1e4c0b3bb3f
Amirvil <amir.vilensky@gmail.com>
2026-05-17 11:23:21 +0100

    Architect some css

--- src/cmp/AppHeader.jsx
+++ src/cmp/AppHeader.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import '../assets/styles/cmp/AppHeader.css'
 
 export const AppHeader = () => {
     return <header className="main-header" >
         <nav className="header-nav container">
-            <h1>TOYS ARE US</h1>
-            <NavLink className="header-link" to="/">Home</NavLink>
-            <NavLink className="header-link" to="/toy">Toys</NavLink>
+            <h1>A'S TOYS</h1>
+            <div className="header-links">
+                <NavLink className="header-link" to="/">Home</NavLink>
+                <NavLink className="header-link" to="/toy">Toys</NavLink>
+            </div>
+
         </nav>
     </header>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import img from '../assets/react.svg'
+import '../assets/styles/pages/Home.css'
 
 
 
 export function Home() {
-    return <section>
-        <h1>Home page for <strong>Amir's</strong> toys!</h1>
-        <div>
-            <img src={img} />
+    return <div className="homePage">
+        <div className="hero">
+            <h1>Where Imagination Comes to Play!</h1>
+            <p>Discover a world of safe, fun, and educational toys designed to spark creativity in children of all ages.</p>
         </div>
-    </section>
+
+        <div>
+            {/* Toy list, Toy filter */}
+        </div>
+
+    </div>
 }
